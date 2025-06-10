d46fb7b73dd448ba79eb043a40f2812e97e64f78
Isa Kaillany <isakaillany123@gmail.com>
2025-06-10 19:48:32 -0300

    feat: adicionar o ciclo de vida OnInit no componente

--- mod-4-life-cycle-hooks/src/app/title/title.component.ts
+++ mod-4-life-cycle-hooks/src/app/title/title.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-title',
     templateUrl: './title.component.html',
     styleUrls: ['./title.component.css'],
 })
-export class TitleComponent {}
+export class TitleComponent implements OnInit {
+    nome: string = 'Isa';
+
+    constructor() {
+        console.log(`Constructor ${this.nome}`);
+    }
+
+    ngOnInit(): void {
+        this.nome = `Ola ${this.nome}`;
+    }
+}
