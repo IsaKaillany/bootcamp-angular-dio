7775b7d15e22cf5b89495b2aeea4c52d183f7d06
Isa Kaillany <isakaillany123@gmail.com>
2025-06-10 19:38:44 -0300

    chore: iniciar módulo 4

---
+++ mod-4-life-cycle-hooks/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { TitleComponent } from './title/title.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    TitleComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
