6a8e7a1522001b6d5337b4e78b643e8cb7fd7612
Isa Kaillany <isakaillany123@gmail.com>
2025-06-10 21:02:10 -0300

    feat: implementar o ciclo de vida OnChange

--- mod-4-life-cycle-hooks/src/app/title/title.component.ts
+++ mod-4-life-cycle-hooks/src/app/title/title.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
 
 @Component({
     selector: 'app-title',
     templateUrl: './title.component.html',
     styleUrls: ['./title.component.css'],
 })
-export class TitleComponent implements OnInit {
-    nome: string = 'Isa';
+export class TitleComponent implements OnInit, OnChanges {
+    @Input() nome: string = '';
 
     constructor() {
         console.log(`Constructor ${this.nome}`);
     }
+    ngOnChanges(): void {
+        console.log(`OnChange ${this.nome}`);
+    }
 
     ngOnInit(): void {
-        this.nome = `Ola ${this.nome}`;
+        console.log(`OnInit ${this.nome}`);
     }
 }
