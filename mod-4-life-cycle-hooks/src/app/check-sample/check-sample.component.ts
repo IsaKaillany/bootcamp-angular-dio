32871d3b54acae449aa6230f356eb410fb47e639
Isa Kaillany <isakaillany123@gmail.com>
2025-06-10 21:39:37 -0300

    feat: implementar o ciclo de vida DoCheck

--- mod-4-life-cycle-hooks/src/app/check-sample/check-sample.component.ts
+++ mod-4-life-cycle-hooks/src/app/check-sample/check-sample.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    DoCheck,
+    AfterContentChecked,
+    AfterContentInit,
+    AfterViewChecked,
+    AfterViewInit,
+} from '@angular/core';
 
 @Component({
-  selector: 'app-check-sample',
-  templateUrl: './check-sample.component.html',
-  styleUrls: ['./check-sample.component.css']
+    selector: 'app-check-sample',
+    templateUrl: './check-sample.component.html',
+    styleUrls: ['./check-sample.component.css'],
 })
-export class CheckSampleComponent {
+export class CheckSampleComponent
+    implements
+        OnInit,
+        DoCheck,
+        AfterContentChecked,
+        AfterContentInit,
+        AfterViewChecked,
+        AfterViewInit
+{
+    constructor() {}
 
+    ngOnInit(): void {
+        console.log(`OnInit`);
+    }
+    ngDoCheck(): void {
+        console.log(`DoCheck`);
+    }
+    ngAfterContentChecked(): void {
+        console.log(`AfterContentChecked`);
+    }
+    ngAfterContentInit(): void {
+        console.log(`AfterContentInit`);
+    }
+    ngAfterViewChecked(): void {
+        console.log(`AfterViewChecked`);
+    }
+    ngAfterViewInit(): void {
+        console.log(`AfterViewInit`);
+    }
 }
